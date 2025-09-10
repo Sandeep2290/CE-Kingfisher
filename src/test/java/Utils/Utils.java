4a89d2a9b2a2fddc3e8dc791c61ec6f673b8a2a8
user_16vzg2 <sandeepramanik@gmail.com>
2025-09-10 10:15:43 +0000

    Added utils class

---
+++ src/test/java/Utils/Utils.java
@@ -0,0 +1,8 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Utils {
+   public static WebDriver driver = new ChromeDriver();
+}
